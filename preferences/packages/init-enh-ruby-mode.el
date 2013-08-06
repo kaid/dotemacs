a35f29cb84f4429ad1e57367257b82eeb1bd45ee
Kaid <kaid@kaid.me>
2013-08-06 13:56:20 +0800

    removed obsolete configuration

--- preferences/packages/init-enh-ruby-mode.el
+++ preferences/packages/init-enh-ruby-mode.el
@@ -2,3 +2,11 @@
           (lambda ()
             (ruby-end-mode 1)
             (autopair-mode 1)))
+
+;; Ruby files
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
